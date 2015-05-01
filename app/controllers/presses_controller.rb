23a7422fe46ac52724b242207448e3e3f52c846d
GagnonStyle <spencer.gagnon95@gmail.com>
2015-05-01 10:42:45 -0400

    Added the ability to delete presses and changed a bunch of icons

--- app/controllers/presses_controller.rb
+++ app/controllers/presses_controller.rb
@@ -9,6 +9,14 @@ class PressesController < ApplicationController
     end
   end
 
+  def destroy
+    Press.find(params[:id]).destroy
+    @presses = Press.all
+    respond_to do |format|
+      format.js
+    end
+  end
+
   private
   def press_params
     params.require(:press).permit(:name, :link)
